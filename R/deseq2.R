cbff2bf5aa70921aaef75cf025fb1174a24f5780
Ido Tamir <ido.tamir@vbcf.ac.at>
2019-06-06 17:34:14 +0200

    changes for singularity rendering

--- R/deseq2.R
+++ R/deseq2.R
@@ -3,18 +3,18 @@
 #' see files in inst/test
 #'
 #' @param configpath path to yaml config
-#'
+#' @param knitdir folder to knit and create output in (caveat singularity. wdir is not writable)
 #'
 #' @export
-analyseFromConfig <- function(configpath){
+analyseFromConfig <- function(configpath, knitdir){
 		deseqconfig <- yaml::read_yaml(configpath)
-		KNITDIR <- getwd()
+		KNITDIR <- knitdir
 		print(deseqconfig)
 
 		form <- bookdown::gitbook( self_contained = TRUE, split_by="none", config = list() )
 
-		rmarkdown::render(system.file("deseq2.Rmd", package="deseq2analysis"), knit_root_dir=KNITDIR,
-                 output_format=form, output_file=deseqconfig$outputname, output_dir=".", clean=TRUE,
+		rmarkdown::render(system.file("deseq2.Rmd", package="deseq2analysis"), knit_root_dir=KNITDIR, intermediates_dir=KNITDIR,
+                 output_format=form, output_file=deseqconfig$outputname, output_dir=KNITDIR, clean=TRUE,
                  params = list(analysis_title = deseqconfig$analysis_title )  )
 
 
@@ -23,8 +23,8 @@ analyseFromConfig <- function(configpath){
 		      paramfile <- paste(outbasefun,"_deseq2parameters.RDS",sep="")
 		      params <- readRDS(paramfile)
     			outbasehtmlfinal <- paste(outbasefun,"_functional_analysis.html",sep="")
-    				rmarkdown::render(system.file("functional_analysis.Rmd", package="deseq2analysis"), knit_root_dir=KNITDIR,
-                	output_format=form, output_file=outbasehtmlfinal, output_dir=".", clean=TRUE, envir = new.env(),
+    				rmarkdown::render(system.file("functional_analysis.Rmd", package="deseq2analysis"), knit_root_dir=KNITDIR, intermediates_dir=KNITDIR,
+                	output_format=form, output_file=outbasehtmlfinal, output_dir=KNITDIR, clean=TRUE, envir = new.env(),
                 	params = list(functional_title = paste(params$comparisontitle, "functional analysis"))
                 	)
 		}
